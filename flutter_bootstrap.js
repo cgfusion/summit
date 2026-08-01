ee75ea12276aed412346e16c61152a4defc3f765
cgfusion <cgfusion@users.noreply.github.com>
2026-08-01 20:21:52 +0000

    deploy: 85f4bc8478725ceb9c95325dfb8f4979d88513f4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1287583473" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2773995524" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
